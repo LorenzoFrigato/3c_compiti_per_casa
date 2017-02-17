5b505f1aea2ba48d8b738cb706c20ba8e1fe36bf
LorenzoFrigato <susye5@libero.it>
2017-02-17 11:04:33 +0100

    es

---
+++ es_22_pag_109.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int inveri (int);
+
+int main (){
+ int n, k;
+ cout << "Inserisci numero: " << endl;
+ cin >> n;
+ k = inverti(n);
+ cout << endl;
+ cout << k;
+}
+
+int inverti (int n){
+
+
+
+}

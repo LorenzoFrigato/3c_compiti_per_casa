96b2db5ddbf61212fe480dc02a8b3d35f0dcc6ac
LorenzoFrigato <susye5@libero.it>
2017-03-29 10:24:40 +0200

    fatto es

--- Selection-sort.cpp
+++ Selection-sort.cpp
@@ -8,6 +8,9 @@ int main(){
     int a[N] = {-2,3,4,1,0,8,6,5,1,4,3,0};
     int dim = 12;
     selection_sort(a, dim);
+    for (int i=0; i<dim; i++){
+        cout<< a[i];
+    }
 
     return 0;
 }
@@ -16,9 +19,9 @@ void selection_sort(int v[], int dim)
 {
     int comodo;
     for(int i = 0; i < dim-1; i++){
-        int minimo=i;
+        int minore=i;
         for (int j = i+1; j < dim; j++){
-            if (v[j] > v[minore]){
+            if (v[j] < v[minore]){
                 minore=j;
             }
             if (j==dim-1){

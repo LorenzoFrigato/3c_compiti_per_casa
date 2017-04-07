2d37bcb68aebb8b189cb958c3858e4f43da09287
LorenzoFrigato <susye5@libero.it>
2017-04-07 11:05:37 +0200

    es

---
+++ stringhe.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+char nome [5];
+nome [0]= 'm';
+nome [1]= 'a';
+nome [2]= 'r';
+nome [3]= 'c';
+nome [4]= 'o';
+nome [5]= '\0';
+cout << nome << endl;
+char cognome [3]= {'f','u','\0'};
+cout << cognome << endl;
+char indirizzo []= "Via bernardi 3/a";
+cout << indirizzo<< endl;
+string telefono = "pippo graziani";
+cout << telefono<< endl;
+
+}
